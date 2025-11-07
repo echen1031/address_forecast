fc98811726d42ef6d6354b45457497ee5cfcec09
Eric Chen <eric87chen@gmail.com>
2025-11-07 15:10:31 -0600

    Update and fix broken tests

--- spec/services/validate_address_service_spec.rb
+++ spec/services/validate_address_service_spec.rb
@@ -2,15 +2,15 @@
 
 require 'rails_helper'
 
-describe AddressCheckService, type: :model do
+describe ValidateAddressService, type: :model do
   before(:each) do
     # Stubbed out address for geocoder
     Geocoder.configure(:lookup => :test)
   end
 
-  describe '#formatted_address' do
-    it 'returns a success celcius temperature' do
-      service = AddressCheckService.new('One Apple Park Way. Cupertino, CA 95014')
+  describe 'ValidateAddressService to handle multiple methods' do
+    it 'successfully calls upon methods' do
+      service = ValidateAddressService.new('One Apple Park Way. Cupertino, CA 95014')
       expect(service).to respond_to(:formatted_address)
       expect(service).to respond_to(:longitude)
       expect(service).to respond_to(:latitude)
